a8407743d490a30f4999229bb246cffae5d7f153
eb3095 <45504889+eb3095@users.noreply.github.com>
2019-09-05 13:07:03 -0400

    Update desktop-chroot-installer.sh

--- desktop/desktop-chroot-installer.sh
+++ desktop/desktop-chroot-installer.sh
@@ -30,7 +30,7 @@ locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
 # Set hostname
-echo iMac > /etc/hostname
+echo arch > /etc/hostname
 
 # Create Hosts file
 echo "127.0.0.1      localhost" >> /etc/hosts
